2cb6ac49516a69c90e122dc7504462fc349d06ce
Lucas <bithome1@outlook.com>
2022-08-11 18:27:13 -0300

    adicionado REACT_APIURL

--- src/services/charactersAPI.js
+++ src/services/charactersAPI.js
@@ -1,8 +1,9 @@
+/* https://app.betrybe.com/course/live-lectures/sd-cohort-19-b#dia-251-deploy-com-heroku */
 import axios from 'axios';
 
 const DEFAULT_TIMEOUT = 30000;
 class CharactersService {
-  constructor({ url = 'http://localhost:3000', timeout = DEFAULT_TIMEOUT }) {
+  constructor({ url = process.env.REACT_APIURL || 'http://localhost:3000', timeout = DEFAULT_TIMEOUT }) {
     this.http = axios.create({
       baseURL: url,
       timeout,
